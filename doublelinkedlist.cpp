c59171938f2de3a9e12baaa1adf2cecff7de39cd
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-13 10:22:39 +0700

    make first node point

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -43,7 +43,7 @@ void addNode()
         // kondisi jika start tidak memiliki nilai atau tidak kosong
         if (START != NULL)
         {
-            
+            START->prev = newNode; // step 4: make the first node point to the 
         }
     }
 }
