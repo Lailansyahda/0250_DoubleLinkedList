9e107bdc02f0bfcb0b689a022fcad35560b8ab0c
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-13 12:00:16 +0700

    prosedur traverse, rectreaverss, dan searchdata

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -162,3 +162,102 @@ bool listEmpty()
 {
     return (START == NULL);
 }
+
+void traverse()
+{
+    if (listEmpty())
+    {
+        cout << "\nList is empty\n";
+    }
+    else
+    {
+        cout << "\n\nRecord in ascending order of roll number are:\n";
+        Node *currentNode = START;
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+    }
+}
+
+void revtraverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecord in ascending order of roll number are: " << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
+void searchData()
+{
+    if (listEmpty() == true)
+    {
+        cout << "\nList is empty" << endl;
+    }
+    Node *prev, *curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to serach: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecord not found " << endl;
+    else
+    {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nNme " << curr->name << endl;
+    }
+}
+
+int main()
+{
+    while (true)
+    {
+        try
+        {
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record to the list" << endl; 
+            cout << "2. Delete a record from the list" << endl;
+            cout << "3. View all records in the ascending order of roll numbers" << endl;
+            cout << "4. View all records in the descending order of roll nunners" << endl; 
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+            cout << "\nEnter your colce (1-6): "; 
+        char ch;
+        cin >> ch;
+
+        switch (ch)
+            {
+            case '1':
+                addNode();
+                break;
+            case '2':
+                deleteNode();
+                break;
+            case '3':
+                traverse();
+                break;
+            case '4':
+                revtraverse();
+                break;
+            case '5':
+                searchData();
+                break;
+            
+            }
+        }
+    }
+}
