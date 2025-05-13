d768cafe299594c5cb5e7553b485e70d5fd1bfd9
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-13 10:09:36 +0700

    mentepkan nilai new node untuk nama

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -23,4 +23,6 @@ void addNode()
     Node *newNode = new Node(); //step 1 buat node baru
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs; // assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name; // assign value to the data field of the new node
 }
