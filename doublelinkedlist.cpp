27cb7de4c4c5632eaf2d189ce27dd107ea4d306b
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-13 10:27:17 +0700

    make new node the first node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -47,5 +47,6 @@ void addNode()
         }
         //memberi nilai prev = null dan start = node baru
         newNode->prev = NULL; // step 5: make the new node point to NULL
+        START = newNode; // step 6: make the new node the first node
     }
 }
